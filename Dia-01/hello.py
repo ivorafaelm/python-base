5c31116e98e01658eb8dd304709ffe949511deec
Ivo Rafael Santos Menezes <ivorafaelm@gmail.com>
2022-05-07 21:22:59 -0300

    Inserindo comentários e definindo shebang

---
+++ Dia-01/hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# A primeira linha indica que a versão do python que será usado é o python3 que está no env (disponível no path do ambiente);
+
+# Este programa imprime Hello World!
+
+print("Hello, World!!")
