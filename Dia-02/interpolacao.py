ea5ce3900d29be7da2a04229a51a23e2c578f5e8
Ivo Rafael Santos Menezes <ivorafaelm@gmail.com>
2022-06-01 10:37:36 -0300

    Adicionando Day-02

---
+++ Dia-02/interpolacao.py
@@ -0,0 +1,28 @@
+email_tmpl = """
+Olá, %(nome)s
+
+Tem interesse em comprar %(produto)s?
+
+Este produto é ótimo para resolver
+%(texto)s
+
+Clique agora em %(link)s
+
+Apenas %(quantidade)d disponíveis!
+
+Preço promocional R$ %(preco).2f
+"""
+
+clientes = ["Maria", "João", "Bruno"]
+
+for cliente in clientes:
+    print(
+        email_tmpl % {
+            "nome": cliente,
+            "produto": "caneta",
+            "texto": "Escrever muito bem",
+            "link": "https://canetaslegais.com",
+            "quantidade": 1,
+            "preco": 50.5,
+        }
+    )
