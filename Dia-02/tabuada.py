8a9788ef28526509059998f792b7d038b4d69b21
Ivo Rafael Santos Menezes <ivorafaelm@gmail.com>
2023-01-05 19:06:08 -0300

    Ajustes nos scripts da aula 02.

--- Dia-02/tabuada.py
+++ Dia-02/tabuada.py
@@ -15,23 +15,14 @@
 __version__ = "0.1.0"
 __author__ = "Ivo Rafael"
 
-template_base = """
-----Tabuada do 2----
-     {bloco}
-
-####################
-""" 
-
-# numeros = [1,2,3,4,5,6,7,8,9,10]
+# O objeto range apresenta uma sequência de forma não inclusive, portanto é 
+# necessário informar um número a mais ao final. No nosso exemplo para irmos 
+# de 1 a 10 é necessário informar os argumentos como (1, 11). Para termos o
+# retorno como lista é necessário efetuar um cast, conforme apresentado abaixo.
 
 numeros = list(range(1,11))
 
-# Para cada numero em numeros
-#for numero in numeros: 
-#    print("Tabuada do: ", numero)
-#    for outro_numero in numeros:
-#        print(numero * outro_numero)
-#    print("------------------")
+# numeros = [1,2,3,4,5,6,7,8,9,10]
 
 for n1 in numeros:
     print("{:-^18}".format(f"Tabuada do {n1}"))
